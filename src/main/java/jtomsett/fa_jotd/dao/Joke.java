faa8c84c6bdbd60629fa94234ad4ace0c5bf3248
jtomsett <jtomsett@gmail.com>
2025-02-20 18:25:35 -0700

    untested controller, tested service layer

--- src/main/java/jtomsett/fa_jotd/dao/Joke.java
+++ src/main/java/jtomsett/fa_jotd/dao/Joke.java
@@ -3,7 +3,6 @@ package jtomsett.fa_jotd.dao;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
